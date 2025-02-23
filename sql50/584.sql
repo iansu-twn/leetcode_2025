fe8057ad43a2563f32a6f85b1e87c0d2a959c399
iansu-twn <iansu.job@gmail.com>
2025-02-24 00:59:29 +0800

    584. Find Customer Referee

---
+++ sql50/584.sql
@@ -0,0 +1,23 @@
+select name
+from customer
+where referee_id != 2 
+or referee_id is null
+
+def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
+    -- method 1
+    return customer[
+            (customer["referee_id"] != 2) |
+            (customer["referee_id"].isna())
+        ][["name"]]
+
+    -- method 2
+    return customer.loc[
+        lambda row: (row["referee_id"] != 2)|
+        (row["referee_id"].isna())
+    ][["name"]]
+
+    -- method 3
+    return customer.loc[
+        (customer["referee_id"] != 2)|
+        (customer["referee_id"].isna())
+    ][["name"]]
