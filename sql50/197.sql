ec23c42ab8ec6f40aee897988ff56a158c3d4ae7
iansu-twn <iansu.job@gmail.com>
2025-02-26 01:14:42 +0800

    add other python and sql solutions

--- sql50/197.sql
+++ sql50/197.sql
@@ -1,8 +1,16 @@
+-- left join
 select w1.id
 from weather w1
 left join weather w2
 on w1.recordDate = date_add(w2.recordDate, interval 1 day)
 where w1.temperature > w2.temperature
+
+-- cross join and set datediff == 1 condition
+select w1.id
+from weather w1
+cross join weather w2
+where datediff(w1.temperature, w2.temperature) = 1
+and w1.temperature > w2.temperature
 
 import pandas as pd
 from datetime import timedelta
@@ -17,3 +25,12 @@ def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
     )
     df = df[df["temperature_x"] > df["temperature_y"]][["id_x"]]
     return df.rename(columns={"id_x": "id"})
+
+    -- method 2
+    -- sort the dataframe by recordDate first
+    -- use diff function to set contidions (it will compare two consecutive data)
+    weather = weather.sort_values(by="recordDate", inplace=True)
+    return weather[
+        (weather.temperature.diff() > 0) &
+        (weather.recordDate.diff().dt.days == 1)
+    ][["id"]]
