7154cc7de89046c279bfa8316e944ce31d7e5920
iansu-twn <iansu.job@gmail.com>
2025-03-01 19:41:26 +0800

    add another sql method

--- sql50/1280.sql
+++ sql50/1280.sql
@@ -8,6 +8,17 @@ and e.subject_name = sub.subject_name
 group by s.student_id, s.student_name, sub.subject_name
 order by s.student_id, sub.subject_name
 
+select s.student_id, s.student_name,
+sub.subject_name, coalesce(t.attended_exams, 0) attended_exams
+from students s
+cross join subjects sub
+left join (
+    select student_id, subject_name, count(*) as attended_exams
+    from examinations
+    group by student_id, subject_name
+)t using (student_id, subject_name)
+order by s.student_id, sub.subject_name
+
 def students_and_examinations(students: pd.DataFrame, subjects: pd.DataFrame, examinations: pd.DataFrame) -> pd.DataFrame:
     df = students.merge(
         subjects,
