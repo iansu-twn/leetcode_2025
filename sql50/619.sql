7a595df670f5b55ab9e5ffa290f1733d5879b494
ian <iansu.job@gmail.com>
2025-03-03 10:32:06 +0000

    619. Biggest Single Number

---
+++ sql50/619.sql
@@ -0,0 +1,18 @@
+select max(num) num
+from (
+    select num
+    from mynumbers
+    group by num
+    having count(*) < 2
+)t
+
+import pandas as pd
+import numpy as np
+def biggest_single_number(my_numbers: pd.DataFrame) -> pd.DataFrame:
+    df = my_numbers.groupby("num").size().reset_index(name="size")
+    df = df[
+        df["size"] == 1
+    ]
+    if df.empty:
+        return pd.DataFrame(data=[{"num": np.nan}])
+    return df[["num"]].sort_values("num", ascending=False)
