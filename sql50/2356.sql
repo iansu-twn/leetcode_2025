a5331766dbe90194c0b7e35c64b852b11cf64527
iansu-twn <iansu.job@gmail.com>
2025-03-03 08:51:04 +0800

    add another agg method

--- sql50/2356.sql
+++ sql50/2356.sql
@@ -2,7 +2,15 @@ select teacher_id, count(distinct subject_id) cnt
 from teacher
 group by teacher_id
 
-df = teacher.groupby("teacher_id").agg({
+def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
+    -- method 1
+    df = teacher.groupby("teacher_id").agg({
         "subject_id": "nunique"
     }).reset_index().rename(columns={"subject_id": "cnt"})
+    return df
+
+    -- method 2
+    df = teacher.groupby("teacher_id").agg(
+        cnt=("subject_id", "nunique")
+    ).reset_index()
     return df
