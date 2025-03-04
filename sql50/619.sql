6c2aaa73952b597d67d0509e71f0c87f9b4bbf64
iansu-twn <iansu.job@gmail.com>
2025-03-04 23:43:41 +0800

    add another python method using drop_duplicated

--- sql50/619.sql
+++ sql50/619.sql
@@ -9,6 +9,7 @@ from (
 import pandas as pd
 import numpy as np
 def biggest_single_number(my_numbers: pd.DataFrame) -> pd.DataFrame:
+    -- method 1
     df = my_numbers.groupby("num").size().reset_index(name="size")
     df = df[
         df["size"] == 1
@@ -16,3 +17,7 @@ def biggest_single_number(my_numbers: pd.DataFrame) -> pd.DataFrame:
     if df.empty:
         return pd.DataFrame(data=[{"num": np.nan}])
     return df.sort_values("num", ascending=False)[["num"]].head(1)
+
+    -- method 2
+    df = my_numbers.num.drop_duplicates(keep=False) -- return a list
+    return pd.DataFrame({"num": [max(df, default=None)]})
