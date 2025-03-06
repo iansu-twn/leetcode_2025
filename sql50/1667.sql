b0cb3b1b6c8083e34cf4c04fb4dca215914bbf5a
iansu-twn <iansu.job@gmail.com>
2025-03-06 21:46:34 +0800

    1667. Fix Names in a Table

---
+++ sql50/1667.sql
@@ -0,0 +1,13 @@
+select user_id,
+concat(upper(substring(name, 1, 1)), lower(substring(name, 2, length(name)))) name
+from users
+order by user_id
+
+select user_id,
+concat(upper(left(name, 1)), lower(right(name, length(name)-1))) name
+from users
+order by user_id
+
+def fix_names(users: pd.DataFrame) -> pd.DataFrame:
+    users["name"] = users["name"].str.capitalize()
+    return users.sort_values("user_id")
