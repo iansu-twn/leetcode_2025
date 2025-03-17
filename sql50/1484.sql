1da34201f27a5bab7fc352b4a2009564548db986
ian <iansu.job@gmail.com>
2025-03-17 10:22:58 +0000

    1484. Group Sold Products By The Date

---
+++ sql50/1484.sql
@@ -0,0 +1,24 @@
+select
+    sell_date, count(distinct product) num_sold,
+    group_concat(distinct product order by product asc separator ',') products
+from activities
+group by sell_date
+order by sell_date
+
+def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
+    activities.drop_duplicates(inplace=True)
+    activities.sort_values(["sell_date", "product"], ascending=True, inplace=True)
+
+    activities_1 = activities.groupby("sell_date").agg({
+        "product": "size"
+    }).reset_index().rename(columns={"product": "num_sold"})
+
+    activities_2 = activities.groupby("sell_date").agg({
+        "product": lambda t: ",".join(t)
+    }).reset_index().rename(columns={"product": "products"})
+    activities = activities_1.merge(
+        activities_2,
+        how="inner",
+        on=["sell_date"]
+    )
+    return activities
