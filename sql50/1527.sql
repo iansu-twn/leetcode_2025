78bbb77a70f3f2b3ba7a79e450d1ed9c70a6d783
ian <iansu.job@gmail.com>
2025-03-17 09:36:38 +0000

    use regexp expression to make sure DIAB1 is a separate word

--- sql50/1527.sql
+++ sql50/1527.sql
@@ -3,8 +3,14 @@ from patients
 where conditions regexp '^DIAB1| DIAB1'
 
 def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
+    -- method 1
     patients = patients[
         (patients["conditions"].str.startswith("DIAB1"))|
         (patients["conditions"].str.contains(" DIAB1"))
+    ]
+
+    -- method 2
+    patients = patients[
+        patients["conditions"].str.contains(r'\bDIAB1')
     ]
     return patients
