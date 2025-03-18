a924342f78eca42fcac06dd88cd303e0827889f6
ian <iansu.job@gmail.com>
2025-03-18 09:14:13 +0000

    add another python method (efficiency)

--- sql50/1174.sql
+++ sql50/1174.sql
@@ -9,6 +9,7 @@ where (customer_id, order_date) in (
 import pandas as pd
 import numpy as np
 def immediate_food_delivery(delivery: pd.DataFrame) -> pd.DataFrame:
+    -- method 1 brute force
     sub = delivery.groupby("customer_id").agg({
         "order_date": "min"
     }).reset_index()
@@ -20,3 +21,10 @@ def immediate_food_delivery(delivery: pd.DataFrame) -> pd.DataFrame:
     delivery["t"] = np.where(delivery["order_date"] == delivery["customer_pref_delivery_date"], 100, 0)
     val = np.average(delivery["t"])
     return pd.DataFrame(data=[{"immediate_percentage": round(val, 2)}])
+
+    -- method 2 sort by order_date and drop_duplicates(by customer_id)
+    delivery = delivery.sort_values("order_date").drop_duplicates(subset="customer_id", keep="first")
+    delivery["val"] = np.where(delivery["order_date"] == delivery["customer_pref_delivery_date"], 100, 0)
+    return pd.DataFrame(data=[{
+        "immediate_percentage": delivery["val"].mean().round(2)
+    }])
