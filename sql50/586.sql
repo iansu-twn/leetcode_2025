ceab3012907c36f7c5590e898b5e84d8e5e908c4
iansu-twn <iansu.job@gmail.com>
2025-03-27 22:42:27 +0800

    use mode function

--- sql50/586.sql
+++ sql50/586.sql
@@ -5,4 +5,8 @@ order by count(*) desc
 limit 1
 
 def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
+    # method 1
     return orders.groupby("customer_number").size().reset_index(name="cnt").sort_values("cnt", ascending=False)[["customer_number"]].head(1)
+
+    # method 2
+    return orders["customer_number"].mode().to_frame()
