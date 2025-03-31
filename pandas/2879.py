55313546c6597881c2101d1ed50817434ad13951
iansu-twn <iansu.job@gmail.com>
2025-03-31 18:53:19 +0800

    use different function to get the same result

--- pandas/2879.py
+++ pandas/2879.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
 def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+    # method 1: head function
     return employees.head(3)
+
+    # method 2: list slicing
+    return employees[:3]
+
+    # method 3: iloc function
+    return employees.iloc[:3, :]
+    
